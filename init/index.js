f252a7398e6951f339b84643f64111343ac13efe
Anurag Ganvir <anuragganvir2019@gmail.com>
2025-02-09 23:07:00 +0530

    final commit

---
+++ init/index.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/NestHive";
+main()
+.then(() => {
+    console.log("connected to DB");
+})
+.catch((err) => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj, owner: "66c48b07444ca0f131fee3fa"}))
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+};
+
+initDB();
